bf832b8f842ad85a6b56205d1ab6e96a306828f2
Ozan Kelkit <53012345+ozankelkit@users.noreply.github.com>
2024-01-08 14:46:18 +0300

    Update model.dart

--- lib/src/model.dart
+++ lib/src/model.dart
@@ -70,34 +70,38 @@ class BackgroundModel with ChangeNotifier {
   ///   fit: BoxFit.cover
   /// )
   /// ```
-  void image(
-      {String? url,
-      String? path,Provider
-      ImageProvider<dynamic>? imageProvider,
-      ColorFilter? colorFilter,
-      BoxFit? fit,
-      AlignmentGeometry alignment = Alignment.center,
-      ImageRepeat repeat = ImageRepeat.noRepeat}) {
-    if ((url ?? path ?? imageProvider) == null)
-      throw ('Either the [imageProvider], [url] or the [path] has to be provided');
-
-    ImageProvider<dynamic> image;
-    if (imageProvider != null)
-      image = imageProvider;
-    else if (path != null)
-      image = AssetImage(path);
-    else
-      image = NetworkImage(url!);
-
-    _image = DecorationImage(
-      image: image as ImageProvider<Object>,
-      colorFilter: colorFilter,
-      fit: fit,
-      alignment: alignment,
-      repeat: repeat,
-    );
-    notifyListeners();
+  void image({
+    String? url,
+    String? path,
+    ImageProvider<dynamic>? imageProvider,
+    ColorFilter? colorFilter,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat
+}) {
+  if (url == null && path == null && imageProvider == null) {
+    throw ('Either the [imageProvider], [url] or the [path] has to be provided');
   }
+
+  ImageProvider<dynamic> image;
+  if (imageProvider != null) {
+    image = imageProvider;
+  } else if (path != null) {
+    image = AssetImage(path);
+  } else {
+    image = NetworkImage(url!); // 'url' is non-null at this point
+  }
+
+  _image = DecorationImage(
+    image: image,
+    colorFilter: colorFilter,
+    fit: fit,
+    alignment: alignment,
+    repeat: repeat,
+  );
+  notifyListeners();
+}
+
 
   void blendMode(BlendMode blendMode) => _blendMode = blendMode;
 }
